428dda4b1145aa992c4971d48c1c8bedc77ed102
Your Name <you@example.com>
2023-05-30 04:06:12 +0200

    DC

--- src/app/(with-layout)/Distribuidor/Agregar/page.jsx
+++ src/app/(with-layout)/Distribuidor/Agregar/page.jsx
@@ -76,7 +76,7 @@ function Home() {
                 <Label htmlFor="">Disponibilidad</Label>
                 <label class="relative inline-flex items-center cursor-pointer">
                     <input type="checkbox" value="" class="sr-only peer" />
-                        <div class="w-11 h-6 bg-emerald-400 peer-focus:outline-none peer-focus:ring-4  rounded-full peer  peer-checked:after:translate-x-full peer-checked:after:border-emerald-400 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-emerald-400 after:border after:rounded-full after:h-5 after:w-5 after:transition-all "></div>
+                    <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-blue-600"></div>
                 </label>
                 </div>
                 <div>
